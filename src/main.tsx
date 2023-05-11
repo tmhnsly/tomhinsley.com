d480c51775c82ca058aebef3a2a660477d1b61fd
Tom <tomhinsley@me.com>
2023-05-12 00:27:06 +0100

    added strict back, changed to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <TooltipProvider delayDuration={400}>
-      <App />
-    </TooltipProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <TooltipProvider delayDuration={400}>
+        <App />
+      </TooltipProvider>
+    </HashRouter>
+  </React.StrictMode>
 );
