e614da628740e1c15ca3328b7f807b4513740194
Tom <tomhinsley@me.com>
2023-06-19 17:21:49 +0100

    commiting for github user auth

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <TooltipProvider delayDuration={400}>
         <App />
       </TooltipProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
